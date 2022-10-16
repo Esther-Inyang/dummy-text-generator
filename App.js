2dc13f45ad6f8d51e28c837ad397e212180bfda4
Esther Inyang <estherinyang07@gmail.com>
2022-10-16 21:29:51 +0100

    first commit here

---
+++ App.js
@@ -0,0 +1,64 @@
+import React, { useState } from "react";
+import data from "./data";
+import "./index.css";
+
+function App() {
+  const [text, setText] = useState([]);
+  const [count, setCount] = useState();
+
+  const submitNum = (e) => {
+    e.preventDefault();
+    let paraCount = parseInt(count);
+
+    if (count <= 0) {
+      paraCount = 1;
+    }
+    if (count >= 8) {
+      paraCount = 8;
+    }
+
+    setText(data.slice(0, paraCount));
+  };
+
+  return (
+    <div className="container">
+      <h1 className="page-title">
+        Tired of boring Lorem Ipsum?{" "}
+        <span className="title-span">Create your 'own' dummy texts..</span>
+      </h1>
+
+      <form onSubmit={submitNum} className="form">
+        <label htmlFor="para" className="para">
+          Paragraphs:
+        </label>
+        <input
+          type="number"
+          name="para"
+          id="para"
+          placeholder="1 - 8"
+          value={count}
+          onChange={(e) => setCount(e.target.value)}
+          className="input"
+        ></input>
+        <button type="submit" className="btn">
+          Generate
+        </button>
+      </form>
+
+      <article className="text-container">
+        <h2 className="para-length">
+          <span className="length">{text.length}</span> paragraphs generated.
+        </h2>
+        {text.map((txt, index) => {
+          return (
+            <p key={index} className="text">
+              {txt}
+            </p>
+          );
+        })}
+      </article>
+    </div>
+  );
+}
+
+export default App;
